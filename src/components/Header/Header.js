2f5f3bd73eb174f6ca3a6fdc6c628b352ec91143
darkzero <darkzero@localhost.localdomain>
2020-10-10 22:04:15 -0500

    second commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components'
-
+import {device} from '../styled/device/device'
 
 const HeaderComponent = styled.header`
     background-color: #205C61;
-    height:3rem;
-    font-size: 20px;
+    height:4rem;
     font-family: 'Sansita Swashed', cursive;
     display: flex;
     margin-right:-15px;
@@ -15,6 +14,21 @@ const HeaderComponent = styled.header`
     align-content:center;
     border-top-right-radius:8px;
     border-top-left-radius:8px;
+
+   h1{
+       font-size:3em;
+
+       @media ${device.tablet}{
+           font-size: 2.7em;
+       }
+       @media ${device.mobilex}{
+           font-size:2.5em;
+       }
+       @media ${device.mobileM}{
+           font-size:2.2em;
+       }
+       
+   }
 `
 const Header =()=> {
 
